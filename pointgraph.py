880befb6b421c562f75c9cdb8aa705acc4e20afe
Kevin Stoner <stonek4rpi@gmail.com>
2017-05-06 20:40:11 -0600

    altered script for actual packet data to show 0s and display a larger graph

--- pointgraph.py
+++ pointgraph.py
@@ -23,17 +23,34 @@ def main():
 
     p = [[],[]]
     p2 = [[],[]]
+    p0 = [[],[]]
+    p20 = [[],[]]
 
     for point in data['points']:
-        p[0].append(point[args.xdata])
-        p[1].append(point[args.ydata])
+        if (point[args.ydata] == 0):
+            p0[0].append(point[args.xdata])
+            p0[1].append(point[args.ydata])
+        else:
+            p[0].append(point[args.xdata])
+            p[1].append(point[args.ydata])
 
     for point in data2['points']:
-        p2[0].append(point[args.xdata])
-        p2[1].append(point[args.ydata])
+        if (point[args.ydata] == 0):
+            p20[0].append(point[args.xdata])
+            p20[1].append(point[args.ydata])
+        else:
+            p2[0].append(point[args.xdata])
+            p2[1].append(point[args.ydata])
 
-    plt.plot(p[0], p[1], 'ro', p2[0], p2[1], 'bo')
-
+    plt.figure(figsize=(12,12), dpi=326)
+    plt.plot(p[0], p[1], 'ro', label=args.file1)
+    plt.plot(p2[0], p2[1], 'bo', label=args.file2)
+    plt.plot(p0[0], p0[1], 'rs', label=(args.file1 + " y = 0"))
+    plt.plot(p20[0], p20[1], 'b^', label=(args.file2 + " y = 0"))
+    plt.xlabel(args.xdata)
+    plt.ylabel(args.ydata)
+    plt.legend()
+    plt.savefig(args.xdata + "_vs_" + args.ydata + ".png")
     plt.show()
 
 main()
